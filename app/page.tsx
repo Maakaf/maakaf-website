222382c7fe67e3ae81f609dc6edfe5175107046b
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-08-15 03:53:28 +0300

    Added The `Paths` component🛤️ 

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Paths from '@/components/Home/Paths'
 export default function Home() {
-  return <main className="mt-5 mb-5">ברוכים הבאים לאתר מעקף!</main>;
+  return (
+    <main className='mt-5 mb-5'>
+      ברוכים הבאים לאתר מעקף!
+      <Paths />
+    </main>
+  )
 }
