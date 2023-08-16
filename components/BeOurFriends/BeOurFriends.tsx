4260a12bfe6d5e824198626c4b9606139268f152
Hilell Elshalem <hilell.elshalem@gmail.com>
2023-08-16 09:02:10 +0300

    add responsive

--- components/BeOurFriends/BeOurFriends.tsx
+++ components/BeOurFriends/BeOurFriends.tsx
@@ -13,7 +13,7 @@ export default function BeOurFriends() {
     return (
         <>
             <h3 className="text-lg">מה תקבלו מלהיות חברים במעקף</h3>
-            <div className="grid grid-cols-2 gap-4 place-content-around place-items-center m-8" >
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 place-content-around place-items-center m-8" >
                 {
                     cards.map((card, index) => (
                         <BenefitCard key={index} index={index} title={card.title} description={card.description}/>
