faeea3d143b73d202d4e8671070daf84a6afa820
Hilell Elshalem <hilell.elshalem@gmail.com>
2023-08-16 08:45:30 +0300

    add style

--- components/BeOurFriends/BenefitCard.tsx
+++ components/BeOurFriends/BenefitCard.tsx
@@ -4,9 +4,14 @@ export default function BenefitCard(props: {
     description: string
 }) {
     return (
-        <div>
-            <h4 className="font-extrabold">{props.index+1} {props.title}</h4>
-            <p className="font-extralight">{props.description}</p>
+        <div className="inline-flex flex-row gap-2 w-3/4">
+            <div>
+                {props.index+1}
+            </div>
+            <div className="grid gap-2">
+                <h4 className="font-extrabold">{props.title}</h4>
+                <p className="font-extralight">{props.description}</p>
+            </div>
         </div>
     )
 }
