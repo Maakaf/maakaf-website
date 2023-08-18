af96d3f52077bd81baac2052eb4e7f5de140940a
Hagar Fisher <hagarfisher@gmail.com>
2023-08-18 12:51:44 +0300

    Add WhatNow section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 // import Quote from '@/components/Home/Quote'
 import TitleSection from '@/components/Home/TitleSection'
+import WhatNow from '@/components/Home/WhatNow'
 
 export default function Home() {
   return (
     <section className='h-full'>
       <TitleSection />
+      <WhatNow />
+
       {/* use after pull request <Quote />*/}
     </section>
   )
