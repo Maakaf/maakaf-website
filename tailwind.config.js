5498744edeb37c7588987421bfebd77ff47ab2e7
rbsPREET <rotem.barsela95@gmail.com>
2023-08-18 11:07:44 +0300

    tailwind config  fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,24 +10,26 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      ...colors,
-      gray: colors.slate,
-      pink: colors.pink,
-      green: colors.teal,
-      purple: colors.indigo,
-      blue: colors.blue,
+    extend: {
+      colors: {
+        ...colors,
+        gray: colors.slate,
+        pink: colors.pink,
+        green: colors.teal,
+        purple: colors.indigo,
+        blue: colors.blue,
 
-      lightBg: colors.slate[100],
-      lightAccBg: colors.blue[100],
-      darkText: colors.slate[950],
-      discordLight: colors.indigo[400],
+        lightBg: colors.slate[100],
+        lightAccBg: colors.blue[100],
+        darkText: colors.slate[950],
+        discordLight: colors.indigo[400],
 
-      // Dark Mode Colors
-      darkBg: colors.slate[950],
-      darkAccBg: colors.slate[900],
-      lightText: colors.slate[50],
-      discordDark: colors.white,
+        // Dark Mode Colors
+        darkBg: colors.slate[950],
+        darkAccBg: colors.slate[900],
+        lightText: colors.slate[50],
+        discordDark: colors.white,
+      },
     },
     fontFamily: {
       birzia: ['Birzia', 'sans-serif'],
