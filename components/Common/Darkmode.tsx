45feb1be0a87d03148bbf5442281667e8a447d03
avikoenig <avik@themis-tech.io>
2023-08-20 19:32:06 +0300

    [main] added hover effect to svg,svgr

--- components/Common/Darkmode.tsx
+++ components/Common/Darkmode.tsx
@@ -1,32 +1,28 @@
-'use client'
-import Image from 'next/image'
+import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes'
+import LightMode from '../../public/images/lightMode.svg'
+import NightMode from '../../public/images/nightMode.svg'
 
 const Darkmode: React.FC = () => {
-  const { setTheme } = useTheme()
+  const [isMounted, setIsMounted] = useState(false);
+  const { setTheme, theme, resolvedTheme } = useTheme()
+  const currentTheme = isMounted ? theme : resolvedTheme; // Use resolvedTheme as a fallback before mount
+  const isDark = (c: typeof currentTheme) => c === 'dark';
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
 
   return (
-    <div className='relative w-6 h-6'>
-      <Image
-        src={'/images/nightMode.svg'}
-        alt='switchDarkMode'
-        className='cursor-pointer absolute inset-0 dark:hidden'
-        width={24}
-        height={24}
-        onClick={() => {
-          setTheme('dark')
-        }}
-      />
-      <Image
-        src={'/images/lightMode.svg'}
-        alt='switchDarkMode'
-        className='cursor-pointer absolute hidden dark:block -z-10 dark:z-10 inset-0'
-        width={24}
-        height={24}
-        onClick={() => {
-          setTheme('light')
-        }}
-      />
+    <div className='relative w-6 h-6 cursor-pointer group'
+      onClick={() =>
+        setTheme(isDark(currentTheme) ? 'light' : 'dark')
+      }
+    >
+      {isDark(currentTheme) ? <LightMode className="stroke-white group-hover:stroke-red-500" /> : <NightMode className="stroke-black group-hover:stroke-green-500" />}
     </div>
   )
 }
