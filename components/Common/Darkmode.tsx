533d5908fa3eb23c2e1e1d055c368563f4f93319
Roy Tennenbaum☕ <roytenen10@gmail.com>
2023-08-21 23:41:04 +0300

    Revert "Added svg w/ gradient using tailwind configs (#31)"
    This reverts commit 4288c53861c1d6c5d1035427a2bf6e966a09f3c6.

--- components/Common/Darkmode.tsx
+++ components/Common/Darkmode.tsx
@@ -1,28 +1,32 @@
-import { useState, useEffect } from 'react';
+'use client'
+import Image from 'next/image'
 import { useTheme } from 'next-themes'
-import LightMode from '../../public/images/lightMode.svg'
-import NightMode from '../../public/images/nightMode.svg'
 
 const Darkmode: React.FC = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  const { setTheme, theme, resolvedTheme } = useTheme()
-  const currentTheme = isMounted ? theme : resolvedTheme; // Use resolvedTheme as a fallback before mount
-  const isDark = (c: typeof currentTheme) => c === 'dark';
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) return null;
-
+  const { setTheme } = useTheme()
 
   return (
-    <div className='relative w-6 h-6 cursor-pointer group'
-      onClick={() =>
-        setTheme(isDark(currentTheme) ? 'light' : 'dark')
-      }
-    >
-      {isDark(currentTheme) ? <LightMode className="stroke-white group-hover:stroke-red-500" /> : <NightMode className="stroke-black group-hover:stroke-green-500" />}
+    <div className='relative w-6 h-6'>
+      <Image
+        src={'/images/nightMode.svg'}
+        alt='switchDarkMode'
+        className='cursor-pointer absolute inset-0 dark:hidden'
+        width={24}
+        height={24}
+        onClick={() => {
+          setTheme('dark')
+        }}
+      />
+      <Image
+        src={'/images/lightMode.svg'}
+        alt='switchDarkMode'
+        className='cursor-pointer absolute hidden dark:block -z-10 dark:z-10 inset-0'
+        width={24}
+        height={24}
+        onClick={() => {
+          setTheme('light')
+        }}
+      />
     </div>
   )
 }
