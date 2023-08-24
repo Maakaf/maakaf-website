75f15a4333639efc6525db8c546858ab0e1433f7
Emily Matheys <emilymatt96@gmail.com>
2023-08-24 18:51:20 +0300

    Check eslint

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server'
 
-export async function GET(_request: NextRequest) {
+export async function GET(request: NextRequest) {
   // Do whatever you want
   return NextResponse.json({ message: 'Hello World' }, { status: 200 });
 }
