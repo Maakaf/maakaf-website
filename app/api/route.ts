553000d0670bdc2062e1eb40cc7ff742e3a55cb9
Emily Matheys <emilymatt96@gmail.com>
2023-08-24 18:41:30 +0300

    I'm an idiot

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,5 +2,5 @@ import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(_request: NextRequest) {
   // Do whatever you want
-  return NextResponse.json({ message: 'Hello World' }, { status: 200 })
+  return NextResponse.json({ message: 'Hello World' }, { status: 200 });
 }
