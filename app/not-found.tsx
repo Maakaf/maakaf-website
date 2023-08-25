3a6c34fa0ae35ba974295abebb817f13159abf1d
develad <develadb@gmail.com>
2023-08-25 10:36:56 +0300

    Adding a catch-all not found 404 page in the app folder

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>לא נמצא</h1>
+      <p>לא יכולנו למצוא את העמוד שביקשתך</p>
+      <Link href='/' className='font-bold text-xl text-cyan-400'>
+        בחזרה לדף הראשי
+      </Link>
+    </div>
+  )
+}
