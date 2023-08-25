79164a3388bc23d9616efe9a1e094ac63ff27ba0
develad <develadb@gmail.com>
2023-08-25 10:43:02 +0300

    Fixing a typo

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div>
       <h1>לא נמצא</h1>
-      <p>לא יכולנו למצוא את העמוד שביקשתך</p>
+      <p>לא יכולנו למצוא את העמוד שביקשת</p>
       <Link href='/' className='font-bold text-xl text-cyan-400'>
         בחזרה לדף הראשי
       </Link>
