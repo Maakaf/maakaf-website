86fb3bdaefe0c96df22865c73dffb22bcebb09c1
develad <develadb@gmail.com>
2023-08-30 22:46:41 +0300

    Restoring the footer fonts to the original Figma design

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -37,13 +37,13 @@ interface FooterLinkProps {
 
 const FooterLink = ({ href, text }: FooterLinkProps) => (
   <Link href={href}>
-    <p className="p-2 text-base font-bold text-center md:text-right">{text}</p>
+    <p className="p-2 text-lg text-center md:text-right">{text}</p>
   </Link>
 );
 
 const Footer: React.FC = () => {
   return (
-    <footer className="grid py-6 mt-8 md:grid-cols-2 border-t-[1px] w-full h-96 md:h-40 border-blue-600 bg-lightBg dark:bg-darkAccBg text-darkText dark:text-lightText">
+    <footer className="grid py-6 mt-8 font-inter md:grid-cols-2 border-t-[1px] w-full h-96 md:h-40 border-blue-600 bg-lightBg dark:bg-darkAccBg text-darkText dark:text-lightText">
       <div className="grid w-full grid-cols-2 2xl:w-2/3 md:px-16 lg:px-24 ">
         {links.map((link, index) => (
           <FooterLink key={index} href={link.href} text={link.text} />
