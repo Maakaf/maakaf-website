6367f9fedf2d4b0a91b297452d8c15d0c56bd5de
Adir Kandel <adirkandel@gmail.com>
2023-09-02 02:16:48 +0300

    creating the members section with avatars inside the home page (#71)

---
+++ components/MembersPics/MembersPics.tsx
@@ -0,0 +1,412 @@
+import Image from 'next/image';
+import Link from 'next/link';
+const MembersPics = () => {
+  return (
+    <div className="darkmode-section pb-60 flex flex-col gap-y-8 overflow-hidden">
+      <div className="grid grid-rows-1 gap-4 -mr-80 sm:-mr-52 md:-mr-32 lg:-mr-0 grid-cols-[repeat(24,65px)] md:grid-cols-[repeat(24,100px)]">
+        <div className="col-span-2 ml-4 bg-gray-700 rounded-e-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar3.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="col-span-2 bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-purple-500 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar2.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar4.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="col-span-2 bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar5.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="col-span-2 bg-gray-700 rounded-s-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+      </div>
+
+      <div className="grid grid-rows-1 gap-4 sm:-mr-72 md:-mr-48 lg:-mr-8 grid-cols-[repeat(24,65px)] md:grid-cols-[repeat(24,100px)]">
+        <div className="col-span-2 sm:col-span-3 ml-4 bg-gray-700 rounded-e-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar5.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="hidden md:block bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-purple-500 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar2.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="hidden md:block bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="col-span-2 bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar3.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-purple-500 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar2.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="col-span-2 bg-gray-700 rounded-s-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+      </div>
+
+      <div className="grid grid-rows-1 gap-4 md:-mr-44 lg:-mr-36 grid-cols-[repeat(24,65px)] md:grid-cols-[repeat(24,100px)]">
+        <div className="hidden md:block sm:col-span-2 md:col-span-2 lg:col-span-3 ml-4 bg-gray-700 rounded-e-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="col-span-6 md:col-span-6 items-center flex">
+          <p className="text-[41px] md:text-[60px] font-bold pr-5">
+            תכירו את חברי{' '}
+            <Link className="text-purple-500 hover:underline" href="/members">
+              הקהילה
+            </Link>
+          </p>
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar5.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar3.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="col-span-3 bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar4.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="col-span-2 bg-gray-700 rounded-s-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+      </div>
+
+      <div className="grid grid-rows-1 gap-4 -mr-8 sm:-mr-52 md:-mr-48 lg:-mr-12 grid-cols-[repeat(24,65px)] md:grid-cols-[repeat(24,100px)]">
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-purple-500 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar2.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar3.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar5.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="col-span-3 bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-purple-500 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar2.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar3.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="col-span-3 bg-gray-700 rounded-s-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+      </div>
+
+      <div className="grid grid-rows-1 gap-4 sm:-mr-72 md:-mr-48 lg:-mr-8 grid-cols-[repeat(24,65px)] md:grid-cols-[repeat(24,100px)]">
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar5.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="sm:col-span-3 bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar4.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="hidden sm:block bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-green-300 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar3.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="hidden md:block bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-purple-500 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar2.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="hidden md:block bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar4.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="col-span-2 bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden"></div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className="bg-gray-700 rounded-[50px] h-[65px] md:h-[100px] overflow-hidden border-blue-600 border">
+          <Image
+            loading="lazy"
+            src="/images/avatars/avatar4.jpg"
+            alt="avatar"
+            className="max-w-full max-h-full"
+            width="100"
+            height="100"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MembersPics;
