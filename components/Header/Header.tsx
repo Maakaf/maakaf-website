a88fff4e97d0de975759aa3d10a25d9d802ea53c
Rotem Bar-Sela <44998514+rotembarsela@users.noreply.github.com>
2023-09-02 00:44:52 +0300

    Header "Dropdown" feature (#70)

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import SizeIndicator from './SizeIndicator';
 import dynamic from 'next/dynamic';
+import { NavDropdown } from './NavDropdown';
 
 const NavRightSide = dynamic(() => import('./NavRightSide'), {
   ssr: false,
@@ -21,25 +22,7 @@ const Header: React.FC = () => {
         <NavRightSide />
         <ul className="hidden md:flex md:gap-10">
           <li className="flex items-center gap-2 cursor-pointer body-roman">
-            <span>קהילה</span>
-            <div className="relative w-[10px] h-[10px]">
-              <Image
-                src={'/images/polygon_white.svg'}
-                alt="polygon"
-                className="absolute inset-0 hidden dark:block -z-10 dark:z-10"
-                width={10}
-                height={10}
-                style={{ width: 10, height: 10 }}
-              />
-              <Image
-                src={'/images/polygon_dark.svg'}
-                className="absolute inset-0 dark:hidden"
-                alt="polygon"
-                width={10}
-                height={10}
-                style={{ width: 10, height: 10 }}
-              />
-            </div>
+            <NavDropdown />
           </li>
           <li className="cursor-pointer body-roman">
             <Link href="/projects">הפרויקטים</Link>
