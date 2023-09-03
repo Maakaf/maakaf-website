c2853cfe4e99a98c514931ba309ff7d18bafbb3f
RoyTennenbaum <roytenen10@gmail.com>
2023-09-03 23:35:20 +0300

    At long last! hover complete

--- components/Common/Darkmode.tsx
+++ components/Common/Darkmode.tsx
@@ -1,27 +1,26 @@
 'use client';
-
+//className="absolute inset-0 hover:stroke-green-200 hover:transition hover:duration-300 hover:ease-in-out"
 import Sun from './Sun.svg';
 import Moon from './Moon.svg';
 import { useTheme } from 'next-themes';
+import { inherits } from 'util';
 
 const Darkmode: React.FC = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="relative w-6 h-6">
-      <button
-        className="cursor-pointer absolute inset-0 bg-transparent border-none p-0 m-0"
-        onClick={() => {
-          setTheme(theme === 'dark' ? 'light' : 'dark');
-        }}
-      >
-        {theme === 'dark' ? (
-          <Sun className="w-24 h-24 absolute inset-0 hover:stroke-green-200 hover:transition hover:duration-300 hover:ease-in-out" />
-        ) : (
-          <Moon className="w-24 h-24 absolute inset-0 hover:stroke-purple-500 hover:transition hover:duration-300 hover:ease-in-out" />
-        )}
-      </button>
-    </div>
+    <button
+      className="w-6 h-6 cursor-pointer inset-0 bg-transparent border-none p-0 m-0"
+      onClick={() => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+      }}
+    >
+      {theme === 'dark' ? (
+        <Sun class="w-[26px] h-[26px] stroke-white stroke-2 hover:stroke-green-200 hover:transition hover:duration-300 hover:ease-in-out" />
+      ) : (
+        <Moon class="w-6 h-6 stroke-black stroke-2 hover:stroke-purple-500 hover:transition hover:duration-300 hover:ease-in-out" />
+      )}
+    </button>
   );
 };
 
