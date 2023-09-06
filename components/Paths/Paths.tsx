87f065c543702d477ff7ade3491faae1fdd0ea83
develad <develadb@gmail.com>
2023-09-06 14:04:19 +0300

    Resolving merge issues #79

--- components/Paths/Paths.tsx
+++ components/Paths/Paths.tsx
@@ -1,5 +1,7 @@
 'use client';
+
 import PathItem from './PathItem';
+import { motion } from 'framer-motion';
 
 const paths = [
   {
@@ -24,6 +26,17 @@ const paths = [
   },
 ];
 
+const variants = {
+  visible: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: index * 0.05,
+    },
+  }),
+  initial: { opacity: 0, y: -100 },
+};
+
 export default function Paths() {
   return (
     <section className="flex flex-col justify-start py-32 darkmode-section">
@@ -37,7 +50,16 @@ export default function Paths() {
 
       <div className="flex flex-row grow flex-wrap justify-evenly gap-y-20 mt-6 py-4 md:gap-y-4 md:py-20 lg:mt-0 lg:flex-row lg:py-20">
         {paths.map((path, index) => (
-          <PathItem key={index} item={path} />
+          <motion.div
+            key={path.title}
+            custom={index}
+            variants={variants}
+            initial="initial"
+            whileInView="visible"
+            viewport={{ once: true, margin: '-200px' }}
+          >
+            <PathItem key={index} item={path} />
+          </motion.div>
         ))}
       </div>
     </section>
