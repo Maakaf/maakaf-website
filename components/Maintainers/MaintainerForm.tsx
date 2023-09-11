3cc8895a8b2c108b91c538b5966ec3a9a4516e01
develad <develadb@gmail.com>
2023-09-11 20:57:20 +0300

    Adjusting styles according to the Figma design

--- components/Maintainers/MaintainerForm.tsx
+++ components/Maintainers/MaintainerForm.tsx
@@ -2,18 +2,23 @@ import Button from '../Common/Button';
 
 const MaintainerForm = () => {
   return (
-    <div className="w-[660px] px-4 max-w-full mx-auto text-center mb-20 relative">
-      <h1 className="mb-3 font-black text-5xl">MAINTAINERS</h1>
-      <div className="flex flex-col gap-5 text-xl mb-5">
-        <p className="font-bold text-2xl">בעל פרויקט ורוצה להצטרף למעקף?</p>
-        <p>
-          טקסט אקראי שמסביר כמה דברים על צירוף פרוייקטים למעקף, מה צריך לעשות
-          כדי להצטרף , איך התהליך עובד בצורה מאוד כללית ומזמינה כי תכף יהיה הסבר
-          יותר מפורט ואין צורך להרחיב כאן.
+    <div className="px-4 mx-auto text-center text-[#020616] dark:text-[#F8FAFC]">
+      <h1 className="mb-3 font-black text-4xl mx-auto md:text-5xl">
+        MAINTAINERS
+      </h1>
+      <div className="w-[316px] md:w-[720px] flex flex-col gap-5 text-xl mb-5">
+        <p className="font-bold text-xl md:text-2xl leading-7">
+          בעל פרויקט ורוצה להצטרף למעקף?
+        </p>
+        <p className="font-medium text-xl md:text-2xl leading-7">
+          טקסט אקראי שמסביר כמה דברים על צירוף פרוייקטים למעקף
+          <br />
+          מה צריך לעשות כדי להצטרף , איך התהליך עובד בצורה מאוד כללית ומזמינה כי
+          תכף יהיה הסבר יותר מפורט ואין צורך להרחיב כאן.
         </p>
       </div>
 
-      <Button className="mx-auto font-inter" />
+      <Button className="mx-auto font-inter my-11" />
     </div>
   );
 };
