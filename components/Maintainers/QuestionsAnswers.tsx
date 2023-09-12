7578c49dc1b9e6873d579b2e95dbf853b8b5c026
develad <develadb@gmail.com>
2023-09-12 14:48:47 +0300

    Small style changes in QuestionsAnswers.tsx

--- components/Maintainers/QuestionsAnswers.tsx
+++ components/Maintainers/QuestionsAnswers.tsx
@@ -32,18 +32,18 @@ const questionsAnswersArray = [
 
 const QuestionsAnswers = () => {
   return (
-    <>
+    <div className="w-full py-8 md:py-16">
       <div className="flex flex-col-reverse md:flex-row self-start md:items-center">
         <div className="flex pt-2 md:p-0">
           <div className="w-[160px] h-[8px] md:w-24 md:h-6 ml-4 bg-blue-500 rounded-full rounded-s-none"></div>
           <div className="w-[8px] h-[8px] md:w-6 md:h-6 ml-4 bg-blue-500 rounded-full"></div>
         </div>
-        <h4 className="w-full text-left pl-4">שאלות ותשובות</h4>
+        <h4 className="text-right pr-4 md:pr-8">שאלות ותשובות</h4>
       </div>
       <div className="w-[316px] md:w-[720px] xl:w-[1240px] mx-auto mt-8">
         <Accordion
           type="single"
-          className="w-full font-medium"
+          className="font-medium"
           defaultValue="item-1"
           collapsible
         >
@@ -63,7 +63,7 @@ const QuestionsAnswers = () => {
           ))}
         </Accordion>
       </div>
-    </>
+    </div>
   );
 };
 
