3f6e3c5b29f58b9f8bb481a633a5dee04c2ab849
develad <develadb@gmail.com>
2023-09-13 09:44:19 +0300

    Removing clsx package

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+import classNames from 'classnames';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: classNames.ArgumentArray): string {
+  return twMerge(classNames(inputs));
 }
