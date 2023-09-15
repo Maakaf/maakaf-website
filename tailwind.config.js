1d188b21ef11fb56dfe603ee717de850e003a9b3
develad <develadb@gmail.com>
2023-09-15 09:30:05 +0300

    Updating container object in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,15 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '2rem',
       screens: {
         '2xl': '1400px',
+      },
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
       },
     },
     extend: {
