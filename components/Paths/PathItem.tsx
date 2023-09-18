8f3ae6c03b1d76fc4f4b9699ca6eff4c0a62336c
Drorka~ <drorpa4@gmail.com>
2023-09-18 11:57:34 +0300

    use classNames for button conditional styling

--- components/Paths/PathItem.tsx
+++ components/Paths/PathItem.tsx
@@ -1,16 +1,26 @@
 'use client';
+
+import classNames from 'classnames';
+
 interface PathProps {
   item: {
     title: string;
     description: string;
     link: { name: string; href: string };
-    btnColorHover: string;
-    btnColorHoverDark: string;
-    btnColorMobile: string;
+    btnColor: string;
   };
 }
 
 export default function PathItem({ item }: PathProps) {
+  const buttonClasses = classNames(
+    'text-base font-normal font-inter px-4 py-2 body-bold w-fit rounded-xl md:bg-lightText md:dark:bg-gray-700',
+    item.btnColor,
+    {
+      [`hover:${item.btnColor}`]: true,
+      [`hover:dark:${item.btnColor}`]: true,
+    }
+  );
+
   return (
     <article className="grid grid-cols-[min-content_1fr] grid-rows-[min-content_1fr] text-center w-72 gap-y-1.5 gap-x-3">
       <div className="md:w-[22px] md:h-[22px] bg-blue-300 rounded-full dark:bg-gray-700 self-center"></div>
@@ -22,11 +32,7 @@ export default function PathItem({ item }: PathProps) {
       <h4 className="capitalize self-center">{item.title}</h4>
       <div className="flex flex-col grow justify-between gap-10 min-h-[128px] items-center ">
         <p className="text-base font-normal font-inter">{item.description}</p>
-        <a
-          className={`text-base font-normal font-inter px-4 py-2 body-bold md:bg-lightText md:dark:bg-gray-700 ${item.btnColorMobile}
-          ${item.btnColorHover} ${item.btnColorHoverDark} w-fit rounded-xl`}
-          href={item.link.href}
-        >
+        <a className={buttonClasses} href={item.link.href}>
           {item.link.name}
         </a>
       </div>
--- components/Paths/Paths.tsx
+++ components/Paths/Paths.tsx
@@ -9,26 +9,20 @@ const paths = [
     description:
       'פעם ראשונה עם קוד פתוח? אל דאגה... בואו נבין ביחד מאיפה להתחיל',
     link: { name: 'צעד ראשון', href: '/' },
-    btnColorHover: 'hover:bg-pink-500',
-    btnColorHoverDark: 'hover:dark:bg-pink-500',
-    btnColorMobile: 'bg-pink-500',
+    btnColor: 'bg-pink-500',
   },
   {
     title: 'Contributors',
     description: 'מכירים כבר את הקונספט ורוצים להצטרף לקהילה? בבקשה',
     link: { name: 'ערוץ דיסקורד', href: '/' },
-    btnColorHover: 'hover:bg-purple-500',
-    btnColorHoverDark: 'hover:dark:bg-purple-500',
-    btnColorMobile: 'bg-purple-500',
+    btnColor: 'bg-purple-500',
   },
   {
     title: 'Maintainers',
     description:
       'יש לכם פרויקט שאתם רוצים לצרף למעקף? מלאו טופס הצטרפות ונתחיל',
     link: { name: 'טופס הצטרפות', href: '/' },
-    btnColorHover: 'hover:bg-green-500',
-    btnColorHoverDark: 'hover:dark:bg-green-500',
-    btnColorMobile: 'bg-green-500',
+    btnColor: 'bg-green-500',
   },
 ];
 
