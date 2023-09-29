3feaf44a8c7ff1dc259621bf17d2d3dd9c65f5ad
Drorka~ <drorpa4@gmail.com>
2023-09-29 13:18:58 +0300

    add sub header text on desktop

--- components/Common/Faqs.tsx
+++ components/Common/Faqs.tsx
@@ -18,7 +18,17 @@ const Faqs = ({ faqs }: FaqsProps) => {
     <>
       <SectionTitle title="שאלות ותשובות" />
 
-      <div className="max-w-[90vw] min-h-[60px] md:mx-20 ">
+      <p className="hidden md:block w-2/3 py-5 px-4 md:mx-20 text-right  text-lg font-Birzia font-light break-words">
+        שווה לנסות את סקטור השאלות תשובות שלנו, ואם התשובה לא כאן, יש לנו את{' '}
+        <span className="underline whitespace-nowrap text-purple-500">
+          <a href="https://discord.com/invite/a2VyCjRk2M" target="_blank">
+            ערוץ הדיסקורד
+          </a>
+        </span>{' '}
+        עם אנשים באמת, אבל באמת, הכי נחמדים שיש ומוכנים לעזור לכל מי שרק צריך.
+      </p>
+
+      <div className=" min-h-[60px] px-4 md:mx-20 ">
         <Accordion
           type="single"
           className="font-medium"
@@ -31,10 +41,10 @@ const Faqs = ({ faqs }: FaqsProps) => {
               value={`item-${index + 1}`}
               className="bg-purple-100 dark:bg-gray-800 rounded-[10px] w-full py-1 px-7 my-4 md:px-12"
             >
-              <AccordionTrigger className="text-sm text-right">
+              <AccordionTrigger className="text-lg text-right">
                 {faq.question}
               </AccordionTrigger>
-              <AccordionContent className="font-inter">
+              <AccordionContent className="text-base font-inter pl-6">
                 {faq.answer}
               </AccordionContent>
             </AccordionItem>
