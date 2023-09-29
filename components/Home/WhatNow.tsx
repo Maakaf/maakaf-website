b9329e2293112c8c8610a0db3bc265600df26fb2
rbsPREET <rotem.barsela95@gmail.com>
2023-09-29 11:56:46 +0300

    WhatNow component flex fix

--- components/Home/WhatNow.tsx
+++ components/Home/WhatNow.tsx
@@ -4,7 +4,7 @@ import SectionTitle from '../Common/SectionTitle';
 function WhatNow() {
   return (
     <div className="relative flex flex-col justify-around gap-3 pb-10 mt-36">
-      <div className="hidden md:flex md:flex-col gap-2 sm:flex-row-reverse sm:justify-end">
+      <div className="hidden sm:flex flex-col gap-2 items-center sm:flex-row-reverse sm:justify-end">
         <h3>אז מה עכשיו?</h3>
         <div className="flex flex-row w-1/2 gap-1">
           <div className="w-56 h-3 bg-blue-500 rounded-full sm:h-6 sm:w-96 rounded-s-none"></div>
@@ -13,7 +13,7 @@ function WhatNow() {
           <div className="w-0 h-3 bg-blue-500 rounded-full sm:h-6 sm:w-56"></div>
         </div>
       </div>
-      <div className="block md:hidden">
+      <div className="block sm:hidden">
         <SectionTitle title="אז מה עכשיו?" />
       </div>
       <div className="flex flex-col gap-7 sm:pr-4 sm:flex-wrap sm:content-end ">
