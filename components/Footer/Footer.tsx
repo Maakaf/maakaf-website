313bc236bdbe3b5c19cf62aee83933a5ee8340b3
Tamir <1tamir198@gmail.com>
2024-03-08 21:03:07 +0200

    Fix the footer to the bottom of the page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -42,25 +42,24 @@ const FooterLink = ({ href, text }: FooterLinkProps) => (
     <p className="p-2 text-lg text-center md:text-right">{text}</p>
   </Link>
 );
-
 const Footer: React.FC = () => {
   return (
-    <footer className="z-10 sticky bottom-0 grid py-6 font-inter md:grid-cols-2 border-t-[1px] w-full h-96 md:h-40 border-blue-600 bg-lightBg dark:bg-darkAccBg text-darkText dark:text-lightText">
-      <div className="grid w-full grid-cols-2 2xl:w-2/3 md:px-16 lg:px-24 ">
+    <footer className="z-10 bottom-0 grid py-6 font-inter md:grid-cols-2 border-t-[1px] w-full border-blue-600 bg-lightBg dark:bg-darkAccBg text-darkText dark:text-lightText">
+      <div className="grid w-full grid-cols-1 md:grid-cols-2 2xl:w-2/3 md:px-2 lg:px-4 md:gap-4 lg:gap-6">
         {links.map((link, index) => (
           <FooterLink key={index} href={link.href} text={link.text} />
         ))}
       </div>
-      <div className="flex flex-col items-center w-full h-auto md:px-24 md:items-end ">
-        <div className="flex flex-col justify-around h-full ">
-          <div className="flex justify-center gap-4 lg:gap-6 ">
+      <div className="flex flex-col items-center w-full h-auto md:px-2 md:items-end md:gap-4 lg:gap-6">
+        <div className="flex flex-col justify-around h-full">
+          <div className="flex justify-center gap-2 md:gap-4 lg:gap-6">
             <GithubIconLink repoUrl="https://github.com/Maakaf/maakaf-website" />
             <FacebookIconLink />
             <LinkedinIconLink />
             <TwitterIconLink />
           </div>
 
-          <div className="flex justify-center w-full gap-10 text-center lg:gap-16 ">
+          <div className="flex justify-center w-full gap-2 text-center md:gap-4 lg:gap-6">
             <Link className="text-sm" href="#">
               תנאי שימוש
             </Link>
