1d9cd1341e03d0cef25968d49642e9ffa8738902
Darkmift <darkmift@gmail.com>
2024-03-09 01:22:16 +0200

    Add API route for fetching projects and implement useFetchProjects hook

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T | ((val: T) => T)) => void] {
+  const [state, setState] = useState<T>(() => {
+    // Initialize the state
+    try {
+      const value = window.localStorage.getItem(key);
+      // Check if the local storage already has any values,
+      // otherwise initialize it with the passed initialValue
+      return value ? JSON.parse(value) : initialValue;
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  const setValue = (value: T | ((val: T) => T)): void => {
+    try {
+      // If the passed value is a callback function,
+      //  then call it with the existing state.
+      const valueToStore = value instanceof Function ? value(state) : value;
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      setState(value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [state, setValue];
+}
+
+export default useLocalStorage;
