dc7b04cf0e90042a2842f974ec1d78ee42da6c1b
Darkmift <darkmift@gmail.com>
2024-03-13 00:13:14 +0200

    Fix useLocalStorage to handle cases where window is not defined

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -6,8 +6,8 @@ function useLocalStorage<T>(
 ): [T, (value: T | ((val: T) => T)) => void] {
   const [state, setState] = useState<T>(() => {
     // Initialize the
-    if (!window) return;
     try {
+      if (!window) return;
       const value = window.localStorage.getItem(key);
       // Check if the local storage already has any values,
       // otherwise initialize it with the passed initialValue
@@ -19,6 +19,7 @@ function useLocalStorage<T>(
 
   const setValue = (value: T | ((val: T) => T)): void => {
     try {
+      if (!window) return;
       // If the passed value is a callback function,
       //  then call it with the existing state.
       const valueToStore = value instanceof Function ? value(state) : value;
