20dd791d9c1e1eef40ea75816b82de676ddaf1b1
Tamir <1tamir198@gmail.com>
2024-03-20 14:20:09 +0200

    Removed non used button component

--- components/Maintainers/MaintainerForm.tsx
+++ components/Maintainers/MaintainerForm.tsx
@@ -1,4 +1,4 @@
-import Button from '../Common/Button';
+import Modal from '../Common/Modals/Modal';
 
 const MaintainerForm = () => {
   return (
@@ -18,7 +18,7 @@ const MaintainerForm = () => {
         </p>
       </div>
 
-      <Button className="mx-auto font-inter my-11" />
+      <button className='text-xs mx-auto mb-9 btn rounded-full px-7 py-2 flex items-center justify-center gap-2 dark:hover:bg-purple-200 dark:hover:text-darkText transition-all w-48'>טופס הצטרפות</button>
     </div>
   );
 };
