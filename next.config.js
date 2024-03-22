f8a9db9b47c8b42faff1f24e282df49889300a24
Darkmift <darkmift@gmail.com>
2024-03-22 12:33:15 +0200

    Remove experimental serverActions from next.config.js not needed

--- next.config.js
+++ next.config.js
@@ -20,9 +20,6 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    serverActions: true,
-  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find(rule =>
