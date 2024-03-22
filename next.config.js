45b32fe9c9aa7d7e16737b5a45ade1ec00335052
Darkmift <darkmift@gmail.com>
2024-03-22 04:42:47 +0200

    Update dependencies and enable experimental server actions

--- next.config.js
+++ next.config.js
@@ -20,6 +20,9 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: true,
+  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find(rule =>
