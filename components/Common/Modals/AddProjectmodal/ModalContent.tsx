f0d34f546b3ae0886f0cc13ecedc6a6b90ea757b
Tamir <1tamir198@gmail.com>
2024-03-27 22:24:38 +0200

    Added direction support

--- components/Common/Modals/AddProjectmodal/ModalContent.tsx
+++ components/Common/Modals/AddProjectmodal/ModalContent.tsx
@@ -1,7 +1,7 @@
 import { addNewProject } from '@/actions/addNewProject';
 import { IAddProjectForm } from '@/types/forms';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import { useForm, SubmitHandler, FieldValues } from 'react-hook-form';
 import { z } from 'zod';
 import { FormTextInput } from '../../Inputs/FormTextInput';
@@ -16,6 +16,9 @@ interface ModalContentProps {
 
 export const ModalContent = ({ closeModal }: ModalContentProps) => {
   const t = useTranslations('maintainers.maintainerForm');
+  const localLang = useLocale();
+  const direction = localLang == 'he' ? 'rtl' : 'ltr'
+  
 
   const schema = z.object({
     fullName: z.string().min(2, t('fullNameError')),
@@ -38,8 +41,9 @@ export const ModalContent = ({ closeModal }: ModalContentProps) => {
 
   return (
     <form
+      dir={localLang === 'he' ? 'rtl' : 'ltr'}
       onSubmit={handleSubmit(onSubmit as SubmitHandler<FieldValues>)}
-      className="p-4 mt-24 dark:bg-[#0F1729] bg-gray-100 dark:text-white  text-black rounded-[12px]"
+      className='p-4 mt-24 dark:bg-[#0F1729] bg-gray-100 dark:text-white  text-black rounded-[12px]'
     >
       <div className="flex justify-between mb-4">
         <p className="text-xl font-bold text-right items-center">
