5e9e0debbbae273e8d30ca615dd59efb81b1b012
Tamir <1tamir198@gmail.com>
2024-03-28 23:05:11 +0200

    Check for links validations

--- tests/newbiesPage.spec.ts
+++ tests/newbiesPage.spec.ts
@@ -16,4 +16,33 @@ test.describe('Test Newbies page', () => {
     await page.goto('http://localhost:3000/he/newbies');
     expect(await page.textContent('h1')).toContain('NEWBIES');
   });
+
+  test('External links contains corrent urls', async ({ page }) => {
+    await page.goto('http://localhost:3000/he/newbies');
+
+    const linksData = [
+      {
+        icon: 'Maakaf_Logo',
+        link: 'https://github.com/UrielOfir/os-practice',
+      },
+      {
+        icon: 'Discord_Logo',
+        link: 'https://discord.com/invite/a2VyCjRk2M',
+      },
+      {
+        icon: 'WhatsApp_Logo',
+        link: 'https://chat.whatsapp.com/E5a59DtSaHNBwnczxVW1FY',
+      },
+    ];
+
+    for (const linkData of linksData) {
+      const linkSelector = `[data-testid="NewbiesExternalLink-${linkData.icon}"]`;
+      const linkExists = await page.waitForSelector(linkSelector);
+      expect(linkExists).toBeTruthy();
+
+      const linkElement = await page.$(linkSelector);
+      const linkURL = await linkElement.getAttribute('href');
+      console.log({ linkURL, linkData: linkData.link });
+    }
+  });
 });
