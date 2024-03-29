6b38c3edf79ac7d3f487761c2540ff36d9a4b4fd
Tamir <1tamir198@gmail.com>
2024-03-29 17:57:51 +0300

    Small change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang={locale} dir="rtl">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
