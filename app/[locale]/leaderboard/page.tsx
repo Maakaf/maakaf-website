a67c59f3bfd6ec70d7ce0160c8a33a04946918c6
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-10 21:46:05 +0300

    make

--- app/[locale]/leaderboard/page.tsx
+++ app/[locale]/leaderboard/page.tsx
@@ -1,6 +1,6 @@
-
+import fetchLeaderboard from '@/actions/fetchLeaderboard';
 import { Metadata } from 'next';
-import { z } from 'zod';
+import { use } from 'react';
 
 export const metadata: Metadata = {
   title: 'לוח מובילים - Leaderboard',
@@ -24,133 +24,33 @@ export const metadata: Metadata = {
 };
 
 const LeaderboardPage: React.FC = () => {
+    const leaderboard = use(fetchLeaderboard());
+    console.log(leaderboard);
 
 
-    const githubWeekStatesSchema = z.object({
-      w: z.number(),
-      a: z.number(),
-      d: z.number(),
-      c: z.number(),
-  });
-
-  const gitHubauthorSchema = z.object({
-        login: z.string(),
-        id: z.number(),
-        node_id: z.string(),
-        // ... (other author properties)
-  });
-
-  const gitHubContributorStatsSchema = z.array(
-    z.object({
-      total: z.number(),
-      weeks: z.array(
-        githubWeekStatesSchema
-      ),
-      author: gitHubauthorSchema,
-    })
-  );
-
-    type GithubContributorStats = z.infer<typeof gitHubContributorStatsSchema>;
-    
-    function calculateTotals(weeks: GithubContributorStats[number]['weeks']) {
-        const totals = weeks.reduce(
-            (acc, week) => {
-                acc.additions += week.a;
-                acc.deletions += week.d;
-                acc.commits += week.c;
-                return acc;
-            },
-            { additions: 0, deletions: 0, commits: 0 }
-        );
-        return totals;
-    };
-    type CalculateTotals = ReturnType<typeof calculateTotals>;
-    type githubExtractDataForLeaderBoard = { 
-        totals: CalculateTotals; 
-        author: GithubContributorStats[number]['author'], 
-        project: { owner: string; repo: string; since: string; until: string; } 
-    };
-
-    const leaderboard = new Map<string, githubExtractDataForLeaderBoard[]>();
-    const repositories = [
-      { owner: 'maakaf', repo: 'maakaf-website' },
-      { owner: 'maakaf', repo: 'Baas-data-provider' },
-    ];
-    const since = '2024-05-05T00:00:00Z';
-    const until = '2024-05-12T00:00:00Z';
-    
-    for (const { owner, repo } of repositories) {
-        const url = `https://api.github.com/repos/${owner}/${repo}/stats/contributors?order=desc&until=${until}&since=${since}`;
-        fetch(url).then((response) => {
-            const parsedData = gitHubContributorStatsSchema.safeParse(response.json());
-            if (!parsedData.success) {
-                return new Map();
-            }
-            const contributorStats: Map<
-                GithubContributorStats[number]['author']["node_id"], 
-                githubExtractDataForLeaderBoard
-            > = new Map();
-            
-            parsedData.data.forEach((contributor) => {
-                const totals = calculateTotals(contributor.weeks);
-                contributorStats.set(
-                contributor.author.node_id, 
-                { 
-                    totals, 
-                    author: contributor.author, 
-                    project: { owner, repo, since, until }
-                });
-            });
-
-            contributorStats.forEach((data) => {
-                if (leaderboard.has(data.author.node_id)) {
-                    leaderboard.get(data.author.node_id)?.push(data);
-                } else {
-                    leaderboard.set(data.author.node_id, [data]);
-                }
-            });
-        });
-    }
-
-    // type Analitycs = {
-    // mambers: {
-    //     name: string, 
-    //     node_id: string, 
-    //     projects_names: {
-    //     url: string, 
-    //     name: string,
-    //     }[], 
-    //     avarar_url: string,
-    //     score: number
-    //     stats: {
-    //     additions: number, 
-    //     deletions: number, 
-    //     commits: number
-    //     },
-    //     data: githubExtractDataForLeaderBoard[]
-    // }[],
-    // since: string,
-    // until: string
-    // }
-console.log({leaderboard});
-
-  return <div>{
-    Array.from(leaderboard).map(([node_id, data]) => {
-        return <div key={node_id}>
-            <div>{data[0].author.login}</div>
-            <div>{data[0].author.node_id}</div>
-            <div>{data[0].totals.additions}</div>
-            <div>{data[0].totals.deletions}</div>
-            <div>{data[0].totals.commits}</div>
+  return (
+    <div>
+        <h1>Leaderboard</h1>
+        <p><span>{leaderboard.until}</span><span>{leaderboard.since}</span></p>
+        <div className='grid gap-3'>
+            {leaderboard.data.map(([node_id, data]) => {
+                return (
+                <div key={node_id} className='flex flex-col'>
+                    <div className='col-span-full'>{data.name}</div>
+                    <div className='flex'>
+                        <div className='text-green-300'><span>{data.stats.additions}</span><span>++</span></div>
+                        <div className='text-red-300'><span>{data.stats.deletions}</span><span>--</span></div>
+                    </div>
+                    <div className='text-slate-300 flex gap-2 '><span>Coomit</span><span>{data.stats.commits}</span></div>
+                </div>
+                );
+            })}
         </div>
-    })
-    }</div>;
+    </div>
+  );
 };
 
 export default LeaderboardPage;
-
-
-
 
 /*
 TODO: Create a the page ui for the leaderboard
