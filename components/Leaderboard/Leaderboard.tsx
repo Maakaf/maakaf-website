9977edfdd95c27b14fa7e6ea0e6a15646d675cb6
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-16 00:03:59 +0300

    first place

--- components/Leaderboard/Leaderboard.tsx
+++ components/Leaderboard/Leaderboard.tsx
@@ -5,12 +5,7 @@ import Image from 'next/image';
 const LeaderboardPage: React.FC<{ leaderboard: Analitycs }> = async props => {
   const since = new Date(props.leaderboard.since);
   const until = new Date(props.leaderboard.until);
-  const options = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const bigScreenFormatter = new Intl.DateTimeFormat('en-US', {
     weekday: 'long',
     year: 'numeric',
@@ -18,7 +13,7 @@ const LeaderboardPage: React.FC<{ leaderboard: Analitycs }> = async props => {
     day: 'numeric',
   });
 
-  const smallScreenFormatter = new Intl.DateTimeFormat('en-US', {
+const smallScreenFormatter = new Intl.DateTimeFormat('en-US', {
     month: 'short',
     day: 'numeric',
     year: 'numeric',
@@ -28,64 +23,26 @@ const LeaderboardPage: React.FC<{ leaderboard: Analitycs }> = async props => {
     until: bigScreenFormatter.format(until),
     smallSince: smallScreenFormatter.format(since),
     smallUntil: smallScreenFormatter.format(until),
-  };
+  }
+  const firstPlace = props.leaderboard.members[0];
+  const secondPlace = props.leaderboard.members[1];
+  const thirdPlace = props.leaderboard.members[2];
 
   return (
-    <div dir="ltr" className="font-inter">
-      <div className="grid gap-5 p-2 sm:grid-cols-2 lg:grid-cols-3 max-w-5xl mx-auto [&>*:nth-child(2)]:col-span-full first:col-span-full [&>*:nth-child(2)]:row-span-2 [&>*:nth-child(2)]:min-h-[200px] [&>*:nth-child(2)]:row-end-4 ">
-        <div className="col-span-full">
+    <div  dir='ltr' className="font-inter">
+      <div className="grid gap-5 p-2 sm:grid-cols-2 lg:grid-cols-3 max-w-5xl mx-auto  ">
+        <div className='col-span-full'>
           <h1>Leaderboard</h1>
           <p>
             {datesText.since} - {datesText.until}
           </p>
         </div>
-        {props.leaderboard.members.map((data, ind) => {
+        <FirstPlacePerson data={firstPlace} place={1} />
+        <DisplayPerson data={secondPlace} place={2} />
+        <DisplayPerson data={thirdPlace} place={3} />
+        {props.leaderboard.members.slice(3, -1).map((data, ind) => {
           return (
-            <div
-              className="flex gap-2 ring ring-slate-500 rounded-md p-4 "
-              key={data.node_id}
-            >
-              <Image
-                src={data.avatar_url}
-                className="rounded-full w-[50px] h-[50px]"
-                alt={data.name}
-                width={50}
-                height={50}
-              />
-              <div className="flex flex-col w-full  ">
-                <a
-                  className="flex justify-between "
-                  href={`https://github.com/${data.name}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <span className="underline first-letter:capitalize">
-                    {data.name}
-                  </span>
-                  <div className="flex gap-2 text-slate-500">
-                    <div>{data.score}</div>
-                    <Code2 />
-                    {ind + 1}#
-                  </div>
-                </a>
-                <div className="flex gap-2">
-                  <div className="flex gap-2 ">
-                    <div className="text-green-300">
-                      <span>{data.stats.additions}</span>
-                      <span>++</span>
-                    </div>
-                    <div className="text-red-300">
-                      <span>{data.stats.deletions}</span>
-                      <span>--</span>
-                    </div>
-                  </div>
-                  <div className="text-slate-300 flex gap-2  ">
-                    <span>Coomit</span>
-                    <span>{data.stats.commits}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <DisplayPerson data={data} key={data.node_id} place={ind + 4} />
           );
         })}
       </div>
@@ -94,3 +51,89 @@ const LeaderboardPage: React.FC<{ leaderboard: Analitycs }> = async props => {
 };
 
 export default LeaderboardPage;
+
+export const DisplayPerson = ({data, place}) => {
+  return (
+    <div className="flex gap-2 ring ring-slate-500 rounded-md p-4 " key={data.node_id}>
+                <Image
+                  src={data.avatar_url}
+                  className="rounded-full w-[50px] h-[50px] "
+                  alt={data.name}
+                  width={50}
+                  height={50}
+                />
+                <div className="flex flex-col w-full  ">
+                  <a 
+                    className='flex justify-between ' 
+                    href={`https://github.com/${data.name}`} 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className='underline first-letter:capitalize' >{data.name}</span><div className='flex gap-2 text-slate-500'><div>{data.score}</div><Code2 />{place}#</div>
+                  </a>
+                  <div className="flex gap-2">
+                    <div className="flex gap-2 ">
+                      <div className="text-green-300">
+                        <span>{data.stats.additions}</span>
+                        <span>++</span>
+                      </div>
+                      <div className="text-red-300">
+                        <span>{data.stats.deletions}</span>
+                        <span>--</span>
+                      </div>
+                    </div>
+                    <div className="text-slate-300 flex gap-2  ">
+                      <span>Coomit</span>
+                      <span>{data.stats.commits}</span>
+                    </div>
+                  </div>
+                </div>
+            </div>
+  )
+}
+
+export const FirstPlacePerson = ({data, place}) => {
+  return (
+    <div className="flex gap-2 ring ring-slate-500 rounded-md p-4 col-span-full " key={data.node_id}>
+                <Image
+                  src={data.avatar_url}
+                  className="rounded-full w-[100px] h-[100px] "
+                  alt={data.name}
+                  width={100}
+                  height={100}
+                />
+                <div className="flex flex-col w-full  ">
+                  <a 
+                    className='flex justify-between ' 
+                    href={`https://github.com/${data.name}`} 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className='underline first-letter:capitalize text-2xl tracking-wider' >
+                      {data.name}
+                    </span>
+                    <div className='flex gap-2 text-slate-500'>
+                    <div>{data.score}</div>
+                      <Code2 />{place}#
+                    </div>
+                  </a>
+                  <div className="flex gap-2">
+                    <div className="flex gap-2 ">
+                      <div className="text-green-300">
+                        <span>{data.stats.additions}</span>
+                        <span>++</span>
+                      </div>
+                      <div className="text-red-300">
+                        <span>{data.stats.deletions}</span>
+                        <span>--</span>
+                      </div>
+                    </div>
+                    <div className="text-slate-300 flex gap-2  ">
+                      <span>Coomit</span>
+                      <span>{data.stats.commits}</span>
+                    </div>
+                  </div>
+                </div>
+            </div>
+  )
+}
