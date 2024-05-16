57b19c9a55613cc0d79b19247dca9554c5d4e224
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-16 23:19:23 +0300

    Refactor LeaderboardPage, improve date formatting, and optimize code

--- components/Leaderboard/Leaderboard.tsx
+++ components/Leaderboard/Leaderboard.tsx
@@ -37,10 +37,11 @@ const LeaderboardPage: React.FC<{ leaderboard: Analitycs }> = async props => {
     <div dir="ltr" className="font-inter">
       <div className="grid gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3 max-w-5xl mx-auto  ">
         <div className="col-span-full">
-          <h1>Leaderboard</h1>
-          <p className='[text-wrap:balance]'>
-            {datesText.since} <span></span> - {datesText.until}
-          </p>
+          <h1 className='pb-8 pt-6'>Leaderboard</h1>
+          <Tabs />
+            <p className="[text-wrap:balance]">
+              {datesText.since} <span className=''>-</span> {datesText.until}
+            </p>
         </div>
         <FirstPlacePerson data={firstPlace} place={1} />
         <DisplaySecoundPerson data={secondPlace} place={2} />
@@ -65,7 +66,7 @@ interface PersonPlace {
 export const DisplayPerson: React.FC<PersonPlace> = ({ data, place }) => {
   return (
     <CardWarper
-      className="flex gap-2 ring-1 ring-indigo-900/80 rounded-md p-4 "
+      className="flex gap-2 ring-1 ring-indigo-900/80 rounded-md p-4 relative "
       key={data.node_id}
     >
       <Image
@@ -87,7 +88,6 @@ export const DisplayPerson: React.FC<PersonPlace> = ({ data, place }) => {
           </span>
           <div className="flex gap-2 text-indigo-700">
             <div>Score: {data.score}</div>
-            {place}#
           </div>
         </a>
         <div className="flex gap-2">
@@ -111,15 +111,19 @@ export const DisplayPerson: React.FC<PersonPlace> = ({ data, place }) => {
   );
 };
 
-export const DisplaySecoundPerson: React.FC<PersonPlace> = ({ data, place }) => {
+export const DisplaySecoundPerson: React.FC<PersonPlace> = ({
+  data,
+  place,
+}) => {
   return (
     <CardWarper
-      className="flex gap-2 ring-1 ring-indigo-900/80 rounded-md p-4 row-span-2 "
+      className="flex gap-2 ring-1 ring-indigo-900/80 rounded-md p-4 row-span-2 relative "
       key={data.node_id}
     >
+      
       <Image
         src={data.avatar_url}
-        className="rounded-full w-[50px] h-[50px] "
+        className="rounded-full w-[50px] h-[50px]"
         alt={data.name}
         width={50}
         height={50}
@@ -136,7 +140,6 @@ export const DisplaySecoundPerson: React.FC<PersonPlace> = ({ data, place }) => 
           </span>
           <div className="flex gap-2 text-indigo-700">
             <div>Score: {data.score}</div>
-            {place}#
           </div>
         </a>
         <div className="flex gap-2 flex-wrap">
@@ -161,6 +164,7 @@ export const DisplaySecoundPerson: React.FC<PersonPlace> = ({ data, place }) => 
 export const DisplayThirdPerson: React.FC<PersonPlace> = ({ data, place }) => {
   return (
     <CardWarper key={data.node_id}>
+      
       <Image
         src={data.avatar_url}
         className="rounded-full w-[50px] h-[50px] "
@@ -201,14 +205,13 @@ export const DisplayThirdPerson: React.FC<PersonPlace> = ({ data, place }) => {
         </div>
       </div>
     </CardWarper>
-
   );
 };
 
 export const FirstPlacePerson: React.FC<PersonPlace> = ({ data, place }) => {
   return (
     <CardWarper
-      className="flex gap-2 ring-1 ring-indigo-900/80 rounded-md p-4 md:p-4 col-span-full  "
+      className="flex gap-2 ring-1 ring-indigo-900/80 rounded-md p-4 md:p-4 col-span-full relative  "
       key={data.node_id}
     >
       <Image
@@ -225,12 +228,11 @@ export const FirstPlacePerson: React.FC<PersonPlace> = ({ data, place }) => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="underline first-letter:capitalize text-2xl tracking-wider">
+          <span className="underline first-letter:capitalize text-2xl tracking-wider text-indigo-50">
             {data.name}
           </span>
           <div className="flex gap-2 text-indigo-700">
             <div>Score: {data.score}</div>
-            {place}#
           </div>
         </a>
         <div className="flex gap-2">
@@ -254,17 +256,56 @@ export const FirstPlacePerson: React.FC<PersonPlace> = ({ data, place }) => {
   );
 };
 
-
 interface TypeCardWarper {
   children: React.ReactNode;
   className?: string;
 }
 
-export const CardWarper: React.FC<TypeCardWarper> = ({ children, className }) => {
-
+export const CardWarper: React.FC<TypeCardWarper> = ({
+  children,
+  className,
+}) => {
   return (
-    <div className={className || "flex gap-2 ring-1 ring-indigo-900/80 rounded-md p-2 "}>
+    <div
+      className={
+        className || 'flex gap-2 ring-1 ring-indigo-900/80 rounded-md p-4 relative'
+      }
+    >
       {children}
     </div>
-  )
+  );
+};
+
+export const Tabs: React.FC = () => {
+  return <div className="flex gap-2">
+  <div className="">
+    <input id="all-times" type="radio" name="time-filter" defaultChecked className="hidden peer" />
+    <label
+      htmlFor="all-times"
+      className="block cursor-pointer select-none rounded-lg py-2 text-2xl text-indigo-700 peer-checked:text-indigo-50 peer-checked:underline duration-200 ease-in-out"
+    >
+      All Times
+    </label>
+  </div>
+
+  <div className="">
+    <input id="weekly" type="radio" name="time-filter" className="hidden peer" />
+    <label
+      htmlFor="weekly"
+      className="block cursor-pointer select-none rounded-lg px-4 py-2 text-2xl text-indigo-700 peer-checked:text-indigo-50 peer-checked:underline duration-200 ease-in-out"
+    >
+      Weekly
+    </label>
+  </div>
+
+  <div className="">
+    <input id="monthly" type="radio" name="time-filter" className="hidden peer" />
+    <label
+      htmlFor="monthly"
+      className="block cursor-pointer select-none rounded-lg px-4 py-2 text-2xl text-indigo-700 peer-checked:text-indigo-50 peer-checked:underline duration-200 ease-in-out"
+    >
+      Monthly
+    </label>
+  </div>
+</div>
 };
