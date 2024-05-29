60cd0281709f374d62929e62d0beeddd4bc22272
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-29 08:57:41 +0300

    refactor: Remove unused code in project.ts

--- types/project.ts
+++ types/project.ts
@@ -17,7 +17,6 @@ export type IProjectsDataResponse = {
 
 const dateToString = z.coerce.date().or(z.string());
 
-// Define the summary schema using Zod
 const SummarySchema = z.object({
   timestamp: z.string(),
   item: z.object({
@@ -59,72 +58,5 @@ const SummarySchema = z.object({
 
 export const SummaryProjectType = SummarySchema;
 
-// Infer the TypeScript type from the summary schema
 export type Project = z.infer<typeof SummarySchema>;
 
-
-// export type Project = {
-//   _id: string;
-//   timestamp: Date;
-//   item: Item;
-//   error: null;
-//   meta: Meta;
-//   __v: number;
-// };
-
-// export type Item = {
-//   data: Data;
-// };
-
-// export type Data = {
-//   repository: Repository;
-// };
-
-// export type Repository = {
-//   name: string;
-//   owner: Owner;
-//   openGraphImageUrl: string;
-//   description: string;
-//   url: string;
-//   createdAt: Date;
-//   updatedAt: Date;
-//   stargazerCount: number;
-//   languages: Languages;
-//   collaborators: null;
-//   contributors: Contributors;
-// };
-
-// export type Contributors = {
-//   edges: ContributorsEdge[];
-// };
-
-// export type ContributorsEdge = {
-//   node: ContributorNode;
-// };
-
-// export type ContributorNode = {
-//   avatarUrl: string;
-//   login: string;
-// };
-
-// export type Languages = {
-//   edges: LanguagesEdge[];
-// };
-
-// export type LanguagesEdge = {
-//   node: LanguageNode;
-// };
-
-// export type LanguageNode = {
-//   name: string;
-// };
-
-// export type Owner = {
-//   id: string;
-//   login: string;
-//   avatarUrl: string;
-// };
-
-// export type Meta = {
-//   link: string;
-// };
